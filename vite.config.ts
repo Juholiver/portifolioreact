67df97351594c27a9b9640de7b9c29fb1ab3d484
juholiver3 <juholiver3@gmail.com>
2026-09-18 17:35:21 -0300

    adcionando cors e mobile section  contato

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -21,11 +20,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-  },
-  optimizeDeps: {
-    include: [
-      'three',
-      '@react-three/fiber',
-    ],
   },
 })
